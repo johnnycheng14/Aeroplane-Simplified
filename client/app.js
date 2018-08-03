fd36f4f79248640e43ff145659624e7812a33226
johnnycheng14 <johnnycheng14@gmail.com>
2018-08-03 17:53:59 -0400

    template board rendering and images

--- client/app.js
+++ client/app.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Board from './components/board'
 
 const App = () => {
   return (
     <div className="main">
       Hello World!!!
-      {/* Board */}
+      <Board />
       {/* Player Turn */}
       {/* Dice Roll BoardComponent */}
       {/* Piece Component x4 */}
---
+++ client/components/board.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react'
+// import Circle from '../../public/img/Circle'
+
+export default class Board extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className="red"><img src='./img/Circle.png' /></div>
+        <div className="yellow"><img src='./img/Circle.png' /></div>
+        <div className="blue"><img src='./img/Circle.png' /></div>
+        <div className="green"><img src='./img/Circle.png' /></div>
+        <div className="red"><img src='./img/Circle.png' /></div>
+        <div className="yellow"><img src='./img/Circle.png' /></div>
+        <div className="green"><img src='./img/Circle.png' /></div>
+        <div className="yellow"><img src='./img/Aeroplane-Yellow.png' /></div>
+        <div className="yellow"><img src='./img/Star.png' /></div>
+        <div className="blue"><img src='./img/Aeroplane-Blue.png' /></div>
+        <div className="blue"><img src='./img/Aeroplane-Blue.png' /></div>
+        <div className="blue"><img src='./img/Circle.png' /></div>
+        <div className="blue"><img src='./img/Circle.png' /></div>
+        <div className="yellow"><img src='./img/Aeroplane-Yellow.png' /></div>
+        <div className="yellow"><img src='./img/Aeroplane-Yellow.png' /></div>
+        <div className="blue"><img src='./img/Aeroplane-Blue.png' /></div>
+        <div className="blue"><img src='./img/Star.png' /></div>
+        <div className="green"><img src='./img/Circle.png' /></div>
+        <div className="yellow"><img src='./img/Circle.png' /></div>
+        <div className="red"><img src='./img/Star.png' /></div>
+        <div className="red"><img src='./img/Aeroplane-Red.png' /></div>
+        <div className="green"><img src='./img/Aeroplane-Green.png' /></div>
+        <div className="green"><img src='./img/Aeroplane-Green.png' /></div>
+        <div className="red"><img src='./img/Circle.png' /></div>
+        <div className="red"><img src='./img/Circle.png' /></div>
+        <div className="red"><img src='./img/Aeroplane-Red.png' /></div>
+        <div className="red"><img src='./img/Aeroplane-Red.png' /></div>
+        <div className="green"><img src='./img/Star.png' /></div>
+        <div className="green"><img src='./img/Circle.png' /></div>
+        <div className="yellow"><img src='./img/Circle.png' /></div>
+        <div className="green"><img src='./img/Circle.png' /></div>
+        <div className="blue"><img src='./img/Circle.png' /></div>
+        <div className="yellow"><img src='./img/Circle.png' /></div>
+        <div className="red"><img src='./img/Aeroplane-Green.png' /></div>
+        <div className="green"><img src='./img/Circle.png' /></div>
+        <div className="blue"><img src='./img/Circle.png' /></div>
+      </div>
+    )
+  }
+}
+ 
